da775ae817c0ed3bb277ed4a3bb1f447e9d12e00
sdblepas <sdblepas+github@gmail.com>
2024-04-11 15:39:26 +0300

    Update BringThemHomeNow.py

--- BringThemHomeNow.py
+++ BringThemHomeNow.py
@@ -1,17 +1,28 @@
+"""Module designed to automate tweeting images for awareness campaigns."""
+
 import os
 import time
-import tweepy
-from shutil import move
 from datetime import datetime
+from shutil import move
+import tweepy
 
-class log:
+class Log:
+    """Class for outputting console logs."""
+    
+    @staticmethod
     def info(text: str):
+        """Logs informational messages."""
         print(f"[Info] {text}")
+    
+    @staticmethod
     def success(text: str):
+        """Logs success messages."""
         print(f"[Success] {text}")
+    
+    @staticmethod
     def error(text: str):
+        """Logs error messages."""
         print(f"[Error] {text}")
-# The class for outputting console logs.
 
 # Twitter API credentials
 consumer_key = config["consumerKey"]
@@ -19,44 +30,31 @@ consumer_secret = config["consumerSecret"]
 access_token = config["accessTokenKey"]
 access_token_secret = config["accessTokenSecret"]
 
-
 # Authenticate to Twitter
-auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret )
-auth.set_access_token(access_token, access_token_secret)
-
-# Create API object
+auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
+log = Log()
 log.success("Connected to the Twitter API")
 log.info(f"User: {api.verify_credentials().screen_name}")
 log.info(f"Followers: {api.verify_credentials().followers_count}")
 time.sleep(1)
 
 # Directory containing images to be tweeted
-image_dir = '/volume1/home/admin/BringThemHome/Otages/'
-published_dir = '/volume1/home/admin/BringThemHome/Otages_pub/'
+IMAGE_DIR = '/volume1/home/admin/BringThemHome/Otages/'
+PUBLISHED_DIR = '/volume1/home/admin/BringThemHome/Otages_pub/'
 
-client = tweepy.Client(consumer_key=consumer_key,
-                       consumer_secret=consumer_secret,
-                       access_token=access_token,
-                       access_token_secret=access_token_secret)
-
-log.info(f"Client: {client.consumer_key}")
-
-from datetime import datetime
-
-def precise_duration_since(start_date_str):
-    start_date = datetime.strptime(start_date_str, "%d %B %Y")
+def precise_duration_since(start_date_str: str) -> tuple:
+    """Calculates the precise duration since a given start date."""
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
     current_date = datetime.now()
     total_days = (current_date - start_date).days
 
-    # Initialize counters
-    years = 0
-    months = 0
-    days = total_days  # Use total_days to keep track of remaining days
-
-    # Calculate years
+    years, months, days = 0, 0, total_days
     while True:
-        next_year_date = start_date.replace(year=start_date.year + 1)
+        try:
+            next_year_date = start_date.replace(year=start_date.year + 1)
+        except ValueError:
+            break
         if next_year_date <= current_date:
             years += 1
             days -= (next_year_date - start_date).days
@@ -64,28 +62,20 @@ def precise_duration_since(start_date_str):
         else:
             break
 
-    # Calculate months
     month_lengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if start_date.year % 4 == 0 and (start_date.year % 100 != 0 or start_date.year % 400 == 0):
-        month_lengths[1] = 29  # February in a leap year
+        month_lengths[1] = 29
 
-    while days >= month_lengths[start_date.month - 1]:
-        days -= month_lengths[start_date.month - 1]
-        months += 1
-        if start_date.month == 12:
-            start_date = start_date.replace(month=1, year=start_date.year + 1)
-            if start_date.year % 4 == 0 and (start_date.year % 100 != 0 or start_date.year % 400 == 0):
-                month_lengths[1] = 29  # Adjust for leap year
-            else:
-                month_lengths[1] = 28
+    for month_length in month_lengths[start_date.month - 1:]:
+        if days >= month_length:
+            days -= month_length
+            months += 1
         else:
-            start_date = start_date.replace(month=start_date.month + 1)
+            break
 
-    # Calculate weeks and days
     weeks = days // 7
-    days_left = days % 7
+    days = days % 7
 
-    # Build the formatted_duration string conditionally with singular and plural forms
     parts = []
     if years > 0:
         parts.append(f"{years} year{'s' if years > 1 else ''}")
@@ -93,38 +83,34 @@ def precise_duration_since(start_date_str):
         parts.append(f"{months} month{'s' if months > 1 else ''}")
     if weeks > 0:
         parts.append(f"{weeks} week{'s' if weeks > 1 else ''}")
-    if days_left > 0:
-        parts.append(f"{days_left} day{'s' if days_left > 1 else ''}")
+    if days > 0:
+        parts.append(f"{days} day{'s' if days > 1 else ''}")
 
-    formatted_duration = ", ".join(parts)
-
+    formatted_duration = ', '.join(parts)
     return total_days, formatted_duration
 
 def tweet_image():
-    total_days, formatted_duration = precise_duration_since("07 October 2023")  # Calculate precise duration
-    total_days += 6  # Add 6 to total_days
-    for image in os.listdir(image_dir):
-        if image.endswith(('.png', '.jpg', '.jpeg')):
-            # Upload image
-            media = api.media_upload(f"{image_dir}/{image}")
-            media_ids = [media.media_id]
-            log.info(f"Media: {media}")
-            filename = image_dir + image
-            log.info(f"Filename: {filename}")
-            # Include both the total number of days and the formatted duration in the tweet
-            tweet = f"#BringThemHomeNow #{total_days}October. Since #7October ({formatted_duration})"
-            #print(f"Tweet would be: {tweet} with media ID: {media_ids[0]}")
-            # Uncomment the next line to post the tweet when ready
-            client.create_tweet(text=tweet, media_ids=[media.media_id], reply_settings='mentionedUsers')
-            move(f"{image_dir}/{image}", f"{published_dir}/{image}")
-            break
+    """Tweets an image with a message including the duration since a specific start date."""
+    images = [img for img in os.listdir(IMAGE_DIR) if img.endswith(('.png', '.jpg', '.jpeg'))]
+    if not images:
+        log.info("No images to tweet. Moving published images"
+                  "back to the source directory.")
+        for img in os.listdir(PUBLISHED_DIR):
+            move(os.path.join(PUBLISHED_DIR, img), os.path.join(IMAGE_DIR, img))
+        images = os.listdir(IMAGE_DIR)
 
+    if images:
+        image_path = os.path.join(IMAGE_DIR, images[0])
+        total_days, formatted_duration = precise_duration_since("2023-10-07")
+        status = f"Bringing them home, day {total_days}. Duration: {formatted_duration}. #BringThemHomeNow"
+        api.update_status_with_media(status=status, filename=image_path)
+        move(image_path, os.path.join(PUBLISHED_DIR, images[0]))
+        log.success(f"Tweeted: {status}")
+    else:
+        log.error("No images available to tweet.")
 
-while True:
-    if not os.listdir(image_dir):  # if image directory is empty
-        # move all images back from published to image directory
-        for image in os.listdir(published_dir):
-            if image.endswith(('.png', '.jpg', '.jpeg')):
-                move(f"{published_dir}/{image}", f"{image_dir}/{image}")
-    tweet_image()
-    time.sleep(3600)
+if __name__ == "__main__":
+    while True:
+        tweet_image()
+        log.info("Waiting for the next tweet.")
+        time.sleep(3600)  # Wait for 1 hour before tweeting again
